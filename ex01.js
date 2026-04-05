74ef8b1a475c3e8229bbadf7cdba96ce4d29c3d5
Moserzzz <141686601+Moserzzz@users.noreply.github.com>
2026-04-05 09:32:46 -0300

    Delete ex01.js

--- ex01.js
+++
@@ -1,5 +0,0 @@
-let nome = "Artur"
-let protocolo = 1234;
-let produto = "Notebook"
-let tempoEsperaMinutos = 30;
-console.log(`Olá ${nome}, seu chamado de número ${protocolo} para o ${produto} foi registrado. O tempo estimado: ${tempoEsperaMinutos} minutos.`)
